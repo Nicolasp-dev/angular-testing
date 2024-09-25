2cbb1391021aac0180f7008bfb3c2d791c52b945
Nicolas <nicpen@nequi.com>
2024-09-24 20:44:58 -0500

    Jasmine: Basic concepts

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,37 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
-
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
     let calculator: CalculatorService,
         loggerSpy: any;
 
-    beforeEach(()=> {
-
-        console.log("Calling beforeEach");
-
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
         TestBed.configureTestingModule({
             providers: [
                 CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
+                {
+                    provide: LoggerService,
+                    useValue: loggerSpy
+                }
             ]
-        });
+        })
 
-        calculator = TestBed.inject(CalculatorService);
-
-    });
-
-    it('should add two numbers', () => {
-
-        console.log("add test");
-
+        calculator = TestBed.inject<CalculatorService>(CalculatorService);
+    })
+    fit('should add two numbers', () => {
         const result = calculator.add(2, 2);
-
+        
         expect(result).toBe(4);
-
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
-
+    })
     it('should subtract two numbers', () => {
-
-        console.log("subtract test");
-
         const result = calculator.subtract(2, 2);
-
-        expect(result).toBe(0, "unexpected subtraction result");
-
+        
+        expect(result).toBe(0);
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
-});
+    })
+})
